34b6609c9a2a2b6c9e235b7a70b4743a9a6b8880
Mihhail <mihhail.kakourov@ivkhk.ee>
2024-10-28 15:05:55 +0200

    updated swagger

--- config/swagger.js
+++ config/swagger.js
@@ -5,7 +5,7 @@ const swaggerOptions = {
     swaggerDefinition: {
         openapi: '3.0.0',
         info: {
-            title: 'My API',
+            title: 'EduShare',
             version: '1.0.0',
             description: 'API documentation',
         },
