b31c62b227ebf609b447e728488ea0dc17edc417
Mihhail Kakourov <mihhail.kakourov@ivkhk.ee>
2024-11-04 02:35:34 +0200

    updated connections

--- models/index.js
+++ models/index.js
@@ -17,6 +17,15 @@ Category.hasMany(Material, { foreignKey: "category_id", as: "materials" });
 User.hasMany(Comment, { foreignKey: "user_id" });
 Comment.belongsTo(User, { foreignKey: "user_id" });
 
+User.hasMany(Rating, {foreignKey: "user_id"});
+Rating.belongsTo(User, {foreignKey:"user_id"});
+
+Material.hasMany(Rating, {foreignKey:"material_id"});
+Rating.belongsTo(Material, {foreignKey:"material_id"});
+
+Material.hasMany(Comment, {foreignKey:"material_id"});
+Comment.belongsTo(Material, {foreignKey:"material_id"})
+
 module.exports = {
   Category,
   Comment,
